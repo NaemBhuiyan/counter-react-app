ec448a3141e92a2b2a2d737c2a229591e7d40fa3
Naem Bhuiyan <hopingboynaem@gmail.com>
2019-11-23 00:56:37 +0600

    Updates

---
+++ precache-manifest.a2b0cf265aa2737e07a05d60bd75f697.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b5fa7d059fa6da9567fe31c6d5160c0",
+    "url": "/counter-react-app/index.html"
+  },
+  {
+    "revision": "6286d4b89c56ab9cbf91",
+    "url": "/counter-react-app/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "60face01ee51e210d973",
+    "url": "/counter-react-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6286d4b89c56ab9cbf91",
+    "url": "/counter-react-app/static/js/2.7505c597.chunk.js"
+  },
+  {
+    "revision": "60face01ee51e210d973",
+    "url": "/counter-react-app/static/js/main.ba0924b3.chunk.js"
+  },
+  {
+    "revision": "eb6b2483624f371a6b14",
+    "url": "/counter-react-app/static/js/runtime-main.7f85e785.js"
+  }
+]);
